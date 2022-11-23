c0778b9a767eb1535013c3bbbe381932369955ee
Anand Kumar M <anand.medidi31@gmail.com>
2022-11-23 20:19:31 +0000

    money-manager-ccbp

---
+++ src/components/MoneyDetails/index.js
@@ -0,0 +1,51 @@
+import './index.css'
+
+const MoneyDetails = props => {
+  const {moneyDetails} = props
+  const {income, expenses} = moneyDetails
+  return (
+    <>
+      <li className="each-money-details-container your-balance-styling">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/money-manager/balance-image.png "
+          className="money-detail-logo"
+          alt="balance"
+        />
+        <div className="heading-amount-type-amount-container">
+          <p className="amount-type">Your Balance</p>
+          <p testid="balanceAmount" className="amount">
+            Rs {income - expenses}
+          </p>
+        </div>
+      </li>
+      <li className="each-money-details-container your-income-styling">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/money-manager/income-image.png "
+          className="money-detail-logo"
+          alt="income"
+        />
+        <div className="heading-amount-type-amount-container">
+          <p className="amount-type">Your Income</p>
+          <p testid="incomeAmount" className="amount">
+            Rs {income}
+          </p>
+        </div>
+      </li>
+      <li className="each-money-details-container your-expenses-styling">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/money-manager/expenses-image.png "
+          className="money-detail-logo"
+          alt="expenses"
+        />
+        <div className="heading-amount-type-amount-container">
+          <p className="amount-type">Your Expenses</p>
+          <p testid="expensesAmount" className="amount">
+            Rs {expenses}
+          </p>
+        </div>
+      </li>
+    </>
+  )
+}
+
+export default MoneyDetails
---
+++ src/components/MoneyManager/index.js
@@ -0,0 +1,205 @@
+import {Component} from 'react'
+import {v4 as uuidv4} from 'uuid'
+import MoneyDetails from '../MoneyDetails'
+import TransactionItem from '../TransactionItem'
+import './index.css'
+
+const transactionTypeOptions = [
+  {
+    optionId: 'INCOME',
+    displayText: 'Income',
+  },
+  {
+    optionId: 'EXPENSES',
+    displayText: 'Expenses',
+  },
+]
+
+export default class MoneyManager extends Component {
+  state = {
+    titleInput: '',
+    amountInput: '',
+    typeId: 'Income',
+    income: 0,
+    expenses: 0,
+    isError: false,
+    areFieldsEmpty: false,
+    transactionsList: [],
+  }
+
+  onTitleInput = e => this.setState({titleInput: e.target.value})
+
+  onAmountInput = e => this.setState({amountInput: parseInt(e.target.value)})
+
+  onOptionSelect = e => this.setState({typeId: e.target.value})
+
+  addTransactionItem = e => {
+    e.preventDefault()
+    const {titleInput, amountInput, typeId, income, expenses} = this.state
+    console.log(this.state)
+    const newTransaction = {
+      id: uuidv4(),
+      title: titleInput,
+      amount: amountInput,
+      type: typeId,
+    }
+    console.log(`Income: ${income}`)
+    console.log(`Expenses: ${expenses}`)
+    console.log(`Difference/Balance: ${income - expenses}`)
+
+    if (typeId === 'Income') {
+      this.setState(prevState => ({
+        transactionsList: [...prevState.transactionsList, newTransaction],
+        income: prevState.income + amountInput,
+        titleInput: '',
+        amountInput: '',
+        isError: false,
+        areFieldsEmpty: false,
+        // balance: prevState.income + amountInput - expenses,
+      }))
+    } else if (typeId === 'Expenses') {
+      this.setState(prevState => ({
+        transactionsList: [...prevState.transactionsList, newTransaction],
+        expenses: prevState.expenses + amountInput,
+        titleInput: '',
+        amountInput: '',
+        isError: false,
+        areFieldsEmpty: false,
+        // balance: income - prevState.expenses - amountInput,
+      }))
+    }
+    // else if (amountInput !== '' && titleInput !== '') {
+    //   this.setState(prevState => ({isError: !prevState.isError}))
+    // } else if (amountInput === '' || titleInput === '') {
+    //   this.setState(prevState => ({areFieldsEmpty: !prevState.areFieldsEmpty}))
+    // }
+  }
+
+  deleteTransaction = (id, type, refund) => {
+    if (type === 'Expenses') {
+      this.setState(prevState => ({
+        transactionsList: prevState.transactionsList.filter(
+          eachTransaction => eachTransaction.id !== id,
+        ),
+        expenses: prevState.expenses - refund,
+      }))
+    } else if (type === 'Income') {
+      this.setState(prevState => ({
+        transactionsList: prevState.transactionsList.filter(
+          eachTransaction => eachTransaction.id !== id,
+        ),
+        income: prevState.income - refund,
+      }))
+    }
+  }
+
+  render() {
+    const {
+      titleInput,
+      amountInput,
+      income,
+      expenses,
+      isError,
+      areFieldsEmpty,
+      transactionsList,
+    } = this.state
+    console.log(this.state)
+    return (
+      <div className="home-container">
+        <div className="money-manager-container">
+          <div className="name-and-welcome-note-container">
+            <h1 className="name">Hi, Richard</h1>
+            <p className="welcome-note">
+              Welcome back to your
+              <span> Money Manager</span>
+            </p>
+          </div>
+          <ul className="money-details-container">
+            <MoneyDetails moneyDetails={{income, expenses}} />
+          </ul>
+          <div className="add-transaction-and-history-container">
+            <form onSubmit={this.addTransactionItem} className="form">
+              <h1 className="add-transaction-heading">Add Transaction</h1>
+              <div className="label-input-container">
+                <label htmlFor="title">TITLE</label>
+                <input
+                  value={titleInput}
+                  onChange={this.onTitleInput}
+                  placeholder="TITLE"
+                  id="title"
+                  type="text"
+                />
+              </div>
+              <div className="label-input-container">
+                <label htmlFor="amount">AMOUNT</label>
+                <input
+                  value={amountInput}
+                  onChange={this.onAmountInput}
+                  placeholder="AMOUNT"
+                  id="amount"
+                  type="text"
+                />
+              </div>
+              <div className="label-input-container">
+                <label htmlFor="select">TYPE</label>
+                <select onChange={this.onOptionSelect} id="select">
+                  <option
+                    defaultChecked
+                    value={transactionTypeOptions[0].displayText}
+                  >
+                    Income
+                  </option>
+                  <option value={transactionTypeOptions[1].displayText}>
+                    Expenses
+                  </option>
+                </select>
+              </div>
+              <div className="add-button-and-warning-container">
+                <button type="submit" className="add-button">
+                  Add
+                </button>
+                {areFieldsEmpty ? (
+                  <p className="insufficient-funds-warning">
+                    *Empty Title/Amount Input
+                  </p>
+                ) : null}
+                {amountInput === 0 ? (
+                  <p className="insufficient-funds-warning">
+                    *Invalid Salary Input
+                  </p>
+                ) : null}
+                {isError ? (
+                  <p className="insufficient-funds-warning">
+                    *Insufficient funds
+                  </p>
+                ) : null}
+              </div>
+            </form>
+            <div className="history-container">
+              <h1 className="history-heading">History</h1>
+              <div className="transactions-list-container">
+                <div className="list-column-headings-container">
+                  <p className="column-headings-title">Title</p>
+                  <p className="column-headings-amount">Amount</p>
+                  <p className="column-headings-type">Type</p>
+                </div>
+
+                {transactionsList.length > 0 ? (
+                  <ul className="special-container-for-only-transactions">
+                    {transactionsList.map(eachTransaction => (
+                      <TransactionItem
+                        deleteTransaction={this.deleteTransaction}
+                        transactionDetails={eachTransaction}
+                        key={eachTransaction.id}
+                      />
+                    ))}
+                  </ul>
+                ) : null}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
